e1a56bbe00ad6571ea8800f08daab147982af3fb
naiguy <naiscalemail@gmail.com>
2020-05-10 22:14:31 -0500

    Creating project

---
+++ MacroDice/Home/HomeScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeScreenViewController.swift
+//  MacroDice
+//
+//  Created by Nawid Ayobi on 5/10/20.
+//  Copyright © 2020 Nawid Ayobi. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
